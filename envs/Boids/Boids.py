df3b5286eb1f577a05fa541e8cbe989bded0a153
Bassel Mabsout <bmabsout@hotmail.com>
2024-04-22 19:39:22 -0400

    added rendering of boids

--- envs/Boids/Boids.py
+++ envs/Boids/Boids.py
@@ -14,12 +14,12 @@ from cmorl.utils.reward_utils import CMORL, RewardFnType
 
 def multi_dim_reward(state, action, env: "BoidsEnv"):
 
-    return rw_vec
+    return np.array([0.0])
 
 
 def composed_reward_fn(state, action, env: "BoidsEnv"):
     
-    return reward
+    return 0.0
 
 
 @tf.function
@@ -35,9 +35,34 @@ def q_composer(q_values):
 
 class BoidsEnv(gym.Env):
 
+    """
+    Description:
+        A flock of boids is simulated in a toroidal environment. Each boid has a position and velocity. The boids
+        are controlled by the user. The user can control the acceleration and angular velocity of each boid.
+
+    Source:
+        This environment corresponds to the boids simulation in the classic computer graphics paper by Craig Reynolds.
+    
+    Observation:
+        Type: Box(4*numBoids)
+        Num     Observation               Min                     Max
+        0       x position                0                       1
+        1       y position                0                       1
+        2       x velocity                -max_speed              max_speed
+        3       y velocity                -max_speed              max_speed
+    
+    Actions:
+        Type: Box(2*numBoids)
+        Num     Action                      Min                     Max
+        0       acceleration                -max_acc                max_acc
+        1       angular velocity            -max_angular_speed      max_angular_speed
+        
+    """
+
+
     metadata = {
         "render_modes": ["human", "rgb_array"],
-        "render_fps": 60,
+        "render_fps": 20,
     }
 
     def __init__(
@@ -63,8 +88,8 @@ class BoidsEnv(gym.Env):
         max_vel = np.array([max_speed, max_speed])
         max_pos = np.array([1.0, 1.0])
         min_pos = np.array([0.0, 0.0])
-        obs_low = np.tile(np.concatenate([min_pos, -max_vel]), self.numBoids)
-        obs_high = np.tile(np.concatenate([max_pos, max_vel]), self.numBoids)
+        obs_low = np.tile(np.concatenate([min_pos, -max_vel]), numBoids)
+        obs_high = np.tile(np.concatenate([max_pos, max_vel]), numBoids)
         self.observation_space = spaces.Box(
             low=obs_low,
             high=obs_high,
@@ -109,9 +134,9 @@ class BoidsEnv(gym.Env):
 
     def step(self, u):
 
-      
+        self.state = self.difference_eq(self.state, u)
         reward = self.cmorl(self.state, u, self)
-        return self._get_obs(), reward, False, False, {}
+        return self.state, reward, False, False, {}
 
     def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
         super().reset(seed=seed)
@@ -121,7 +146,58 @@ class BoidsEnv(gym.Env):
         return self.state, {}
 
     def render(self):
-        exit(1)
+        if self.render_mode is None:
+            assert self.spec is not None
+            gym.logger.warn(
+                "You are calling render method without specifying any render mode. "
+                "You can specify the render_mode at initialization, "
+                f'e.g. gym.make("{self.spec.id}", render_mode="rgb_array")'
+            )
+            return
+
+        try:
+            import pygame
+            from pygame import gfxdraw
+        except ImportError as e:
+            raise DependencyNotInstalled(
+                "pygame is not installed, run `pip install gymnasium[classic-control]`"
+            ) from e
+
+        if self.screen is None:
+            pygame.init()
+            if self.render_mode == "human":
+                pygame.display.init()
+                self.screen = pygame.display.set_mode(
+                    (self.screen_dim, self.screen_dim)
+                )
+            else:  # mode in "rgb_array"
+                self.screen = pygame.Surface((self.screen_dim, self.screen_dim))
+        if self.clock is None:
+            self.clock = pygame.time.Clock()
+        
+        self.surf = pygame.Surface((self.screen_dim, self.screen_dim))
+        self.surf.fill((255, 255, 255))
+
+        for i in range(0, self.state.shape[0], 4):
+            x = int(self.state[i]*self.screen_dim)
+            y = int(self.state[i+1]*self.screen_dim)
+            x2 = int(x + self.state[i+2]*self.screen_dim)
+            y2 = int(y + self.state[i+3]*self.screen_dim)
+            pygame.draw.line(self.surf, (0, 0, 0), (x, y), (x2, y2), 1)
+            pygame.gfxdraw.filled_circle(self.surf, x, y, 5, (0, 0, 0))
+        
+        self.surf = pygame.transform.flip(self.surf, False, True)
+        self.screen.blit(self.surf, (0, 0))
+
+        if self.render_mode == "human":
+            pygame.event.pump()
+            self.clock.tick(self.metadata["render_fps"])
+            pygame.display.flip()
+        else:  # mode == "rgb_array":
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(self.screen)), axes=(1, 0, 2)
+            )
+        
 
     def close(self):
         if self.screen is not None:
@@ -135,3 +211,13 @@ class BoidsEnv(gym.Env):
 def normed_angular_distance(a, b):
     diff = (b - a + np.pi) % (2 * np.pi) - np.pi
     return np.abs(diff + 2 * np.pi if diff < -np.pi else diff) / np.pi
+
+
+if __name__ == "__main__":
+    env = BoidsEnv(numBoids=1, render_mode="human")
+    env.reset()
+    env.state = tf.constant([0.5, 0.5, 0.0, 0.0], dtype=tf.float32)
+    for _ in range(1000):
+        env.step(env.action_space.sample())
+        env.render()
+    env.close()
