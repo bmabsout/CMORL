cbca8bba2b2a54c22e38d036cceeda65cd5ea49b
Bassel Mabsout <bmabsout@hotmail.com>
2024-05-04 19:14:57 -0400

    made p_mean numerically stable for large p!

--- cmorl/utils/loss_composition.py
+++ cmorl/utils/loss_composition.py
@@ -38,7 +38,7 @@ def tf_pop(tensor, axis):
 
 
 @tf.function
-def p_mean(l, p: float, slack=1e-15, default_val=0.0, axis=None, dtype=tf.float64):
+def p_mean(l, p: float, slack=1e-7, default_val=0.0, axis=None, dtype=tf.float64):
     """
     The Generalized mean
     l: a tensor of elements we would like to compute the p_mean with respect to, elements must be > 0.0
@@ -47,18 +47,24 @@ def p_mean(l, p: float, slack=1e-15, default_val=0.0, axis=None, dtype=tf.float6
     axis: axis or axese to collapse the pmean with respect to, None would collapse all
     https://www.wolframcloud.com/obj/26a59837-536e-4e9e-8ed1-b1f7e6b58377
     """
-    l = tf.cast(tf.convert_to_tensor(l), dtype)
-    p = tf.cast(p, l.dtype)
-    slack = tf.cast(slack, l.dtype)
-    default_val = tf.cast(default_val, l.dtype)
-    min_val = tf.constant(1e-5, dtype = l.dtype)
+    slack = tf.cast(slack, dtype)
+    l = tf.cast(tf.convert_to_tensor(l), dtype) + slack
+    p = tf.cast(p, dtype)
+    default_val = tf.cast(default_val, dtype)
+    min_val = tf.constant(1e-5, dtype=dtype)
     p = tf.where(tf.abs(p) < min_val, -min_val if p < 0.0 else min_val, p)
+    
+    stabilizer = tf.reduce_min(l) if p < 1.0 else tf.reduce_max(l)
+    stabilized_l = l/stabilizer # stabilize the values to prevent overflow or underflow
+    
 
     return tf.cond(tf.reduce_prod(tf.shape(l)) == 0 # condition if an empty array is fed in
         , lambda: tf.broadcast_to(default_val, tf_pop(tf.shape(l), axis)) if axis else default_val
-        , lambda: tf.reduce_mean((l + slack)**p, axis=axis))**(1.0/p) - slack
-        # tf.debugging.assert_greater_equal(l, 0.0)
+        , lambda: (tf.reduce_mean(stabilized_l**p, axis=axis))**(1.0/p) - slack)*stabilizer
 
+# @tf.custom_gradient
+# def fixed_grad_p_mean(l, p: float, slack=1e-15, default_val=0.0, axis=None, dtype=tf.float64):
+#     return p_mean(l, p, slack, default_val, axis, dtype), lambda dy: dy * tf.ones_like(l), None, None, None
 
 @tf.function
 def p_to_min(l, p=0, q=0):
@@ -115,9 +121,19 @@ def move_towards_range(x, min, max):
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     with tf.GradientTape() as gt:
-        x = tf.Variable(tf.linspace(-2.0, 2.0, 100))
-        y = move_towards_range(x, 0.0, 1.0)
+        samples = 100
+        x = tf.Variable(tf.linspace(0.1, 1.0, samples)**5.0)
+        randos = tf.broadcast_to(tf.expand_dims(np.random.rand(50)**5.0, 1), (50, samples))
+        a = tf.concat([tf.cast(tf.expand_dims(x, 0), tf.float64), tf.cast(randos, tf.float64)], 0)
+        y = p_mean(a, -10.0, axis=0)
+        print(x.shape)
+        print(y.shape)
+    #     x = tf.Variable(tf.linspace(-2.0, 2.0, 100))
+    #     y = move_towards_range(x, 0.0, 1.0)
 
-    plt.plot(x, gt.gradient(y,x))
-    plt.plot(x, y)
+    plt.plot(x, gt.gradient(y,x), label="grad")
+    plt.plot(x, y, label="y")
+    plt.plot(x, np.min(a, axis=0), label="min")
+    plt.plot(x, np.max(a, axis=0), label="max")
+    plt.legend()
     plt.show()
