7530f6903d34ce656d21a7e502e0f1e418f501a4
Bassel Mabsout <bmabsout@hotmail.com>
2024-08-25 14:55:05 -0400

    added multi-seed hypersearch

--- cmorl/hyper_search.py
+++ cmorl/hyper_search.py
@@ -16,26 +16,32 @@ def parse_hypersearch_args(args=None):
     )
 
     parser.add_argument(
+        "--num_seeds", type=int, default=1, help="number of seeds to search per random hyperparam selection"
+    )
+
+    parser.add_argument(
         "--experiment_name", "-n", type=str, default="hypersearch", help="experiment name"
     )
     return parser.parse_known_args(args)
 
-def random_args_generator(hyperseed, n=10):
-    for seed in range(n):
+def random_args_generator(hyperseed, num_searches, num_seeds):
+    for hyper_search_seed in range(num_searches):
         np_random, _ = seeding.np_random(hyperseed)
-        np_random, _ = seeding.np_random(int(np_random.integers(1e10) + seed))
-        yield [
-            # "-p_b", str(np_random.normal(0.0, 1)),
-            # "-p_o", str(np_random.normal(0.0, 1)),
-            # "-q_b", str(np_random.normal(0.0, 1)),
-            # "-q_o", str(np_random.normal(0.0, 1)),
-            "-q_d", str(np_random.uniform(0.1, 7.0)),
-            "--act_noise", str(2**np_random.uniform(-5, -1.5)),
-            # "--batch_size", str(int(np_random.uniform(30, 500))),
-            # "--gamma", str(1.0 - 10**np_random.uniform(-3, -1)),
-            # "--replay_size", str(int(10**np_random.uniform(4, 6))),
-            # "--polyak", str(1.0 - 10**np_random.uniform(-3, -1)),
-        ]
+        np_random, _ = seeding.np_random(int(np_random.integers(1e10) + hyper_search_seed))
+        for seed in range(num_seeds):
+            yield [
+                # "-p_b", str(np_random.normal(0.0, 1)),
+                # "-p_o", str(np_random.normal(0.0, 1)),
+                # "-q_b", str(np_random.normal(0.0, 1)),
+                # "-q_o", str(np_random.normal(0.0, 1)),
+                # "-q_d", str(np_random.uniform(0.1, 7.0)),
+                # "--act_noise", str(2**np_random.uniform(-5, -1.5)),
+                # "--batch_size", str(int(np_random.uniform(30, 500))),
+                # "--gamma", str(1.0 - 10**np_random.uniform(-3, -1)),
+                # "--replay_size", str(int(10**np_random.uniform(4, 6))),
+                # "--polyak", str(1.0 - 10**np_random.uniform(-3, -1)),
+                "--seed", str(seed),
+            ]
 
 def run_training(env_name, args):
     try:
